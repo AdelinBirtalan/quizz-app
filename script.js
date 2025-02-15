dc82a6338e8d70c4c33e4abb729389ac45c9c5c5
unknown <adelinbirtalan3@gmail.com>
2025-02-15 03:41:57 -0800

    Default commit

---
+++ script.js
@@ -0,0 +1,126 @@
+const questions = [
+    {
+        question: "Where is the Mona Lisa?",
+        answers: [
+            { text: "Vatican Museums", correct: false},
+            { text: "Louvre Museum", correct: true},
+            { text: "The British Museum", correct: false},
+            { text: "National Gallery of Art", correct: false},
+        ]
+    },
+    {
+        question: "What are elephant tusks made of?",
+        answers: [
+            { text: "Plastic", correct: false},
+            { text: "Fiber", correct: false},
+            { text: "Leather", correct: false},
+            { text: "Ivory", correct: true},
+        ]
+    },
+    {
+        question: "What two primary colors can be mixed to make purple?",
+        answers: [
+            { text: "Red and Blue", correct: true},
+            { text: "Yellow and Green", correct: false},
+            { text: "Red and Yellow", correct: false},
+            { text: "Blue and Green", correct: false},
+        ]
+    },
+    {
+        question: "How many legs do insects typically have?",
+        answers: [
+            { text: "Six", correct: true},
+            { text: "Eight", correct: false},
+            { text: "Four", correct: false},
+            { text: "Ten", correct: false},
+        ]
+    },
+    
+
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again"
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    };
+};
+
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz();
